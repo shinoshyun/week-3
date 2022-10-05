752d49481a045672042d8216acd648d1cb33e91f
shinoshyun <8367shyun@gmail.com>
2022-10-05 09:58:53 +0800

    Update file.py

--- file.py
+++ file.py
@@ -1,6 +1,3 @@
-from datetime import date
-from itertools import count
-from tkinter import N
 import urllib.request as reguest
 import json
 src = "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json"
@@ -11,7 +8,7 @@ clist = data["result"]["results"]
 count = 0
 with open("data.csv", "w", encoding="utf-8") as file:
     for information in clist:
-        jpg = information["file"].split("jpg")
+        jpg = information["file"].split("https")
         date = information["xpostDate"].split("/")
         # 程式碼示例演示如何使用列表推導方法將列表從字串轉換為整數
         int_list = [int(i) for i in date]
@@ -20,4 +17,4 @@ with open("data.csv", "w", encoding="utf-8") as file:
                        information["address"][5:8]+", " +
                        information["longitude"] + ", " +
                        information["latitude"]+", " +
-                       jpg[0]+"jpg"+"\n")
+                       "https"+jpg[1]+"\n")
