d023f4ce514ad03f8d58c8f1dbe52c9c744a4214
shinoshyun <8367shyun@gmail.com>
2022-10-07 20:45:11 +0800

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -1,5 +1,10 @@
+import bs4
 import urllib.request as req
-url = "https://www.ptt.cc/bbs/movie/index.html"
-with req.urlopen(url) as response:
+url = "https://www.ptt.cc/bbs/index.html"
+request = req.Request(url, headers={
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"})
+with req.urlopen(request) as response:
     data = response.read().decode("utf-8")
-print(data)
+
+root = bs4.BeautifulSoup(data, "html.parser")
+print(root.title)
