933b123a23d782c22c865ea9e89b685427cc76f8
shinoshyun <8367shyun@gmail.com>
2022-10-08 01:14:05 +0800

    Update file.py

--- file.py
+++ file.py
@@ -1,3 +1,4 @@
+import re
 import urllib.request as request
 import json
 src = "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json"
@@ -8,8 +9,8 @@ clist = data["result"]["results"]
 count = 0
 with open("data.csv", "w", encoding="utf-8") as file:
     for information in clist:
-        jpg = information["file"].split("JPG|jpg")
-        date = information["xpostDate"].split("/")
+        jpg = re.split("jpg|JPG",information["file"])
+        date = re.split("/",information["xpostDate"])
         # 程式碼示例演示如何使用列表推導方法將列表從字串轉換為整數
         int_list = [int(i) for i in date]
         if int_list[0] >= 2015:
@@ -18,3 +19,4 @@ with open("data.csv", "w", encoding="utf-8") as file:
                        information["longitude"] + ", " +
                        information["latitude"]+", " +
                        jpg[0]+"jpg"+"\n")
+
